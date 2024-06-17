51b9f4e4092afd6f7c736a7c284e73462a35a005
Lê Nhung <89861867+Nhung472@users.noreply.github.com>
2024-06-17 22:10:18 +0700

    unit2 done

--- webpage_ws/unit_02/main.js
+++ webpage_ws/unit_02/main.js
@@ -1,8 +1,38 @@
-let vueApp = new Vue({
-    el: "#vueApp",
-    data: {
-        menu_title: 'My menu title',
-        main_title: 'Main title, from Vue!!',
-        phrase: "This is the left side of my web page. It occupies 55% of the total width. Let's practice a bit more using VUE to change the content on the web page."
+var app = new Vue({
+    el: '#app',
+
+    data:{
+        connected: false,
+        ros: null,
+        ws_address: 'ws://3.91.38.82:9090',
+        logs: [],
+    },
+
+    methods: {
+        connect: function(){
+            //console.log('connect to rosbridge server!!')
+            this.logs.unshift('connect to rosbridge server!!')
+            //var ros = new ROSLIB
+            this.ros = new ROSLIB.Ros({
+                url: this.ws_address
+            })
+            this.ros.on('connection', () => {
+                this.connected = true
+                this.logs.unshift('Connected!')
+                //console.log('Connected!')
+            })
+            this.ros.on('error', (error) => {
+                this.logs.unshift('Error connecting to websocket server')
+                //console.log('Error connecting to websocket server: ', error)
+            })
+            this.ros.on('close', () => {
+                this.connected = false
+                this.logs.unshift('Connection to websocket server closed.')
+                //console.log('Connection to sebsocket server closed.')
+            })
+        },
+        disconnect: function(){
+            this.ros.close()
+        },
     }
 })
